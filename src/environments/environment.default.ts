7caea2ca11a99a4ac76b5590b9b31f4d14d98765
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-06-03 12:26:32 +0300

    first commit

---
+++ src/environments/environment.default.ts
@@ -0,0 +1,13 @@
+export const defaultEnvironment = {
+    firebase: {
+      apiKey: "AIzaSyBMdSjBTksUo4g14d88owZ3r3oXEqgzfXg",
+      authDomain: "last-sogno.firebaseapp.com",
+      projectId: "last-sogno",
+      storageBucket: "last-sogno.appspot.com",
+      messagingSenderId: "602550191391",
+      appId: "1:602550191391:web:ac459f8542e3ee32b3dc2a",
+      measurementId: "G-JXLKJVGZ6J"
+    },
+    production: false
+  };
+  
